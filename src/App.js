77c537450156bf5977765dd600b32d52a373e5f0
José Gatica <contacto@josegatica.cl>
2024-10-08 12:28:21 -0300

    Agregando routes al proyecto

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import logo from './assets/guitar.png';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <NavBar />
+
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/item" element={<ItemDetailContainer />} />
+        <Route path="*" element={<h1>404 ups!</h1>} />
+      </Routes>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <ItemListContainer mensaje="Este mensaje será mostrado en ItemListContainer" parrafo="HOLA HOLA HOLA Este es un párrafo" />
+
+      </BrowserRouter>
       <Footer />
 
-      
     </div>
   );
 }
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,13 @@
+import './ItemDetailContainer.css';
+
+const ItemDetailContainer = () => {
+    return (
+        <div className='CartWidget-app'>
+            <h2>
+                ToneHub
+            </h2>
+        </div>
+    )
+}
+
+export default ItemDetailContainer
