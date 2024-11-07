ac71c55a52e17886b7220dd58691b82f405528a2
José Gatica <contacto@josegatica.cl>
2024-11-06 21:59:51 -0300

    restaurado proyecto a versión de anterior entrega - redirigiré un poco el desarrollo y el consumo de datos

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,32 +1,49 @@
-import { collection, getDocs } from 'firebase/firestore';
-import './ItemList.css';
-import { db } from '../../config/firebase.js';
-import { useEffect, useState } from 'react';
 
-const ItemList = () => {
-    const [items, setItems] = useState([]);
-    useEffect(() => {
-        const collectionRef = collection(db,"products");
-    getDocs(collectionRef).then(snaps => {
-        const { docs } = snaps;
-        const list = docs.map(doc => ({ ...doc.data(), id: doc.id }));
-        console.log(list);
-        setItems(list);
-    });
-    }, []);
+import './ItemList.css'
+import Item from '../Item/Item'
 
-    return (
-    <>
-        <h1>Item List</h1>
-        {items.map((item) => (
-            <div key={item.id}>
-                <p>Nombre: {item.description}</p>
-                <p>Precio: {item.price}</p>
-                {/* <button onClick={() => addCart(item.id)}>Agregar a Carrito</button> */}
-            </div>
-        ))}
-    </>
-    );
-};
+const ItemList = ({ products }) => {
+    return(
+        <div className='ListGroup'>
+            {products.map(prod => <Item key={prod.id} {...prod} />)}
+        </div>
+    )
+}
 
-export default ItemList;
+export default ItemList
+
+
+// import { collection, getDocs } from 'firebase/firestore';
+// import './ItemList.css';
+// import { db } from '../../config/firebase.js';
+// import { useEffect, useState } from 'react';
+
+// const ItemList = () => {
+//     const [items, setItems] = useState([]);
+//     useEffect(() => {
+//         const collectionRef = collection(db,"Aj8zVaq3SsgTj5zdFrDj");
+//     getDocs(collectionRef).then(snaps => {
+//         const { docs } = snaps;
+//         const list = docs.map(doc => ({ ...doc.data(), id: doc.id }));
+//         console.log(list);
+//         setItems(list);
+//     });
+//     }, []);
+
+//     return (
+//     <>
+//         <h1>Item List</h1>
+//         <div className='ListGroup'>
+//         {items.map((item) => (
+//             <div key={item.id}>
+//                 <p>Nombre: {item.description}</p>
+//                 <p>Precio: {item.price}</p>
+//                 {/* <button onClick={() => addCart(item.id)}>Agregar a Carrito</button> */}
+//             </div>
+//         ))}
+//         </div>
+//     </>
+//     );
+// };
+
+// export default ItemList;
